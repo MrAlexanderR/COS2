5f1d9f9a51dcab0391e8ef5a1ca0f8ca6c52f1fc
MrAlexanderR <oktyabr184@mail.ru>
2020-11-21 00:13:23 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+
+import java.io.File;
+
+public class Main {
+    static final String PATH_TO_FILE = "D:\\Моя папка\\Учёба тип\\7 семестр\\ЦОС\\Лаба 2\\Wolf.bmp";
+    private static final float[] coefficients = new float[] {1.2f, 3f, 7f, 21f};
+
+    public static void main(String[] args) {
+        File output = new File(PATH_TO_FILE);
+        if(output.exists()) {
+            BilinearInterpolation task = new BilinearInterpolation(output);
+            for (float coef: coefficients) {
+                File newFile = task.compress(coef);
+                task.resize(coef, newFile);
+            }
+        }else {
+            System.out.println("Enter: correct image path");
+        }
+    }
+}
